702c6cddf439944b3a69285dbca7a22e6148f5f8
Banifest <banifest@gmail.com>
2019-03-07 13:59:31 +0300

    End of second task

---
+++ HW03/task2/script.js
@@ -0,0 +1,7 @@
+document.getElementById("bt1").onclick = () => {
+    window.location.replace("http://google.com");
+};
+
+document.getElementById("bt2").onclick = () => {
+    window.document.body.innerText = "";
+};
