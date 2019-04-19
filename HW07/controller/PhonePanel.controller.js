d232ea8173204bacada81974ee3023ca167ddc4c
Aliaksandr Martyniuk <banifest@gmail.com>
2019-04-19 19:21:31 +0300

    Example succ

--- HW07/controller/PhonePanel.controller.js
+++ HW07/controller/PhonePanel.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
     return Controller.extend("sap.ui.hw07.PhonePanel", {
-        onInit : function () {
+        onInit: async function () {
+            let data = await $.ajax(`https://services.odata.org/V4/(S(i2qsmldwlbxcrupw01rxfa3l))/TripPinServiceRW/Photos/`, {
+                method: "GET",
+                contentType: "application/json",
+            });
+            console.log(data.value);
+            let oViewmodel = new JSONModel({photos: data.value});
+            this.getView().setModel(oViewmodel);
 
         },
     });
